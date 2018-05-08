0ff75949ece62b2de1cb6156d7fe7a0987182ebb
Moses Liu <moses.s.liu@gmail.com>
2018-05-07 18:12:05 -0700

    commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    config.assets.compile = true 
     config.serve_static_assets = true
     config.assets.digest = true
 
