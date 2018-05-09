1ffe46a0e91b708dfc2d50762de3c65a31eec814
Moses Liu <moses.s.liu@gmail.com>
2018-05-09 00:14:01 -0700

    figured out what is going on with rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,21 @@
 class ArticlesController < ApplicationController
   def new
+    puts "hello"
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to(article_path(@article))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find params[:id]
   end
 
   private
