4adf1c1b63b221b2555a2eb8e461e6744640e7f0
Moses Liu <moses.s.liu@gmail.com>
2018-05-10 00:40:05 -0700

    added bootstrap and improved UI

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-  validates :title, presence:true, length: {minimum: 5, maximum: 50}
-  validates :description, presence:true, length: {minimum: 10, maximum: 100}
+  validates :title, presence:true, length: {minimum: 5}
+  validates :description, presence:true, length: {minimum: 10}
 
 end
