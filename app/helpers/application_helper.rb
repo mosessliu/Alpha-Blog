1ef74d32009568b6eab728df41a229d33ad9e92f
Moses Liu <moses.s.liu@gmail.com>
2018-05-10 23:09:18 -0700

    added user show page with user's list of articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    img_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    return image_tag(img_src, alt: user.username, class: "img-circle")
+  end
 end
