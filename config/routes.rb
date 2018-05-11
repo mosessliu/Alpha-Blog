c9c3391ed5bbf43cd908c5efff7a90ff737dfd8b
Moses Liu <moses.s.liu@gmail.com>
2018-05-10 22:14:51 -0700

    added show, edit, and update for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  get 'signup', to: 'users#new'
+  resources 'users', except: [:new]
+
   get 'about', to: 'pages#about'
   resources :articles
   
